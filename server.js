1088130ce681c4cae46a06db17c1bbf013fa5a46
Clare <cuicuiaaa0307@gmail.com>
2020-04-30 19:52:46 +1000

    server first push

---
+++ server.js
@@ -0,0 +1,34 @@
+const express =require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+})
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json()
+})
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
