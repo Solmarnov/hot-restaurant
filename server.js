53203a91b3e8f9120f680f1e2fc4a724384ebd5a
Solmarnov <solmarnov@gmail.com>
2020-04-30 21:06:02 +1000

    Update

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const reservations = [];
+const waitList = [];
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -24,8 +27,24 @@ app.get("/api/tables", function(req, res) {
 })
 
 app.get("/api/waitlist", function(req, res) {
-    return res.json()
+    return res.json();
 })
+
+app.post("/reserve", function(req, res) {
+    console.log(req.body);
+    const reservation = req.body;
+
+    if (reservations.length > 5) {
+        waitList.push(reservation);
+
+        res.send("Sorry, we're fully booked. We've put you on our waiting list.")
+      } else {
+           
+        reservations.push(reservation);
+
+        res.send("Thanks for booking a table with us!");
+      };
+});
 
 
 app.listen(PORT, function() {
